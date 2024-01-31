637ac45c92fedfa43f7a94174eb1ab8d4d7a21e4
Yasith Ishara Perera <yasithishara@gmail.com>
2024-01-31 09:26:05 +0530

    Initial commit

---
+++ src/HelloStdStreams.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+void main(){
+    System.out.println("Hello streams");
+    System.err.println("Hello error");
+    Scanner scanner=new Scanner(System.in);
+    System.out.print("Enter : ");
+    String input = scanner.nextLine();
+    System.out.println("You entered "+input);
+}
