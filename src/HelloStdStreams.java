9c84de5746099d2a38a37c08bc5aa105e809e9dd
Yasith Ishara Perera <yasithishara@gmail.com>
2024-01-31 09:28:11 +0530

    one line commented

--- src/HelloStdStreams.java
+++ src/HelloStdStreams.java
@@ -4,7 +4,7 @@ void main(){
     System.out.println("Hello streams");
     System.err.println("Hello error");
     Scanner scanner=new Scanner(System.in);
-    System.out.print("Enter : ");
+    //System.out.print("Enter : ");
     String input = scanner.nextLine();
     System.out.println("You entered "+input);
 }
